72370bf58b7178299650c1708616e0f72dbb3a0b
Rupinder Kumar <kumarrupinder99@gmail.com>
2026-02-07 18:30:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,42 +12,30 @@ $(document).ready(function () {
   });
 
   function isValidEmail(email) {
-    var emailRegex = /^[a-zA-Z0-9._]+@[a-zA-Z]+\.[a-zA-Z]{2,}$/;
-    return emailRegex.test(email);
+    var pattern = /^[a-zA-Z0-9._]+@[a-zA-Z]+\.[a-zA-Z]{2,}$/;
+    return pattern.test(email);
   }
 
   $("#submitbutton").click(function () {
     var error = "";
 
     if ($("#name").val() == "") {
-      error += "<p>Name field is empty</p>";
+      error += "<p>Name field is required</p>";
     }
 
     if ($("#email").val() == "") {
-      error += "<p>Email field is empty</p>";
-    }
-
-    if ($("#phone").val() == "") {
-      error += "<p>Phone field is empty</p>";
-    }
-
-    if ($("#password").val() == "") {
-      error += "<p>Password field is empty</p>";
-    }
-
-    if ($("#confirmpassword").val() == "") {
-      error += "<p>Confirm Password field is empty</p>";
-    }
-
-    if ($("#email").val() != "") {
+      error += "<p>Email field is required</p>";
+    } else {
       if (!isValidEmail($("#email").val())) {
         error += "<p>Email format is not correct</p>";
       }
     }
 
-    if ($("#phone").val() != "") {
+    if ($("#phone").val() == "") {
+      error += "<p>Phone field is required</p>";
+    } else {
       if (isNaN($("#phone").val())) {
-        error += "<p>Phone must be numbers only</p>";
+        error += "<p>Phone must contain numbers only</p>";
       }
 
       if ($("#phone").val().length != 10) {
@@ -55,30 +43,28 @@ $(document).ready(function () {
       }
     }
 
-    if ($("#password").val() != "" && $("#confirmpassword").val() != "") {
-      if ($("#password").val() != $("#confirmpassword").val()) {
-        error += "<p>Passwords do not match</p>";
-      }
-    }
-
-    if ($("#password").val() != "") {
+    if ($("#password").val() == "") {
+      error += "<p>Password field is required</p>";
+    } else {
       if ($("#password").val().length < 8) {
         error += "<p>Password must be at least 8 characters</p>";
       }
     }
 
+    if ($("#confirmpassword").val() == "") {
+      error += "<p>Confirm Password field is required</p>";
+    } else {
+      if ($("#password").val() != $("#confirmpassword").val()) {
+        error += "<p>Passwords do not match</p>";
+      }
+    }
+
     if (error == "") {
       $("#msg").html("Form submitted successfully");
-      $("#msg").removeClass("error");
-      $("#msg").addClass("success");
-      $("#msg").show();
-
-      $("#myForm")[0].reset();
+      $("#msg").attr("class", "success");
     } else {
       $("#msg").html(error);
-      $("#msg").removeClass("success");
-      $("#msg").addClass("error");
-      $("#msg").show();
+      $("#msg").attr("class", "error");
     }
   });
 });
